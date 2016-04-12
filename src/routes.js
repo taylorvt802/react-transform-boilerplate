32cd94c08f481ac68a858a75eab904f4265ab8bf
Taylor Traverse <taylorvt802@gmail.com>
2016-04-12 17:19:36 -0400

    Add Comment App to Routes

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import Workspace from './Workspace';
 import App from './App';
 import Home from './Home';
+import CommentApp from './CommentApp';
 
 export default (
   <Route path="/" component={Workspace}>
     <IndexRoute path="/" component={Home}/>
     <Route path='/counter' component={App}/>
+    <Route path='/comment' component={CommentApp}/>
   </Route>
 )
